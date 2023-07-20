e1bac7e66472146aa7ccd857e8c27f3281b01ba2
Rogue-Agent7 ___ <jackrussfell24@outlook.com>
2023-07-20 16:13:21 +0000

    changing name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ TODO:  Change these things along with:
   - favicon.ico in /public
  */
 const font = Inter({subsets: ['latin']})
-const title = 'Luke\'s Site';
-const description = 'This the Tech Journey template site';
+const title = 'Jacksons\'s Site';
+const description = 'This is my Personal Site';
 const links = [
   {title: 'Pokemon', href: '/pokemon'},
   {title: 'Rick and Morty', href: '/rick-and-morty'}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,14 +21,13 @@ export default function Home() {
       <div>
         <section>
           <p className={'prose'}>
-            Hello, I&apos;m <b>Luke</b>. I&apos;m a software engineer in love
-            with front end development. This is the starter template for Tech Journey!
+            Hello my name is <b>Jackson Fell</b>, Welcome, to the blog.
           </p>
           <div className={'my-4'}>
             <i>
               Check out the repo{' '}
               <button className={'btn btn-primary'}>
-                <Link href={'https://github.com/ludu12/tech-journey-blog'}>
+                <Link href={'https://github.com/Rogue-Agent7/tech-journey-blog'}>
                   <span className={'text-primary-content'}>
                     here
                   </span>
